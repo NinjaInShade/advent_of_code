92d0f80a3b0fc9fec74f38d2d97977b24f02c077
Leon Michalak <leonmichalak6@gmail.com>
2020-12-22 22:02:21 +0000

    Day 3 part 1 code completed and correct

--- day_3/day_3.js
+++ day_3/day_3.js
@@ -2,10 +2,30 @@ const parse_file = require("../parse_file.js");
 
 const input_array = parse_file("day_3", "day_3_input.txt");
 
-function day_3_part_1(input) {}
+function day_3_part_1(input) {
+  // Variable to keep track of tree's we hit
+  let trees_encountered = 0;
+
+  // Loop through each element in array (or each line from the input file)
+  for (i in input) {
+    // In challenge it says pattern on each line repeats many times, i'll do 5 for arguments sake.
+    let repeated_line = input[i];
+    for (j = 0; j < 5; j++) {
+      repeated_line = `${repeated_line}${repeated_line}`;
+    }
+
+    // Going down 1 is simulated automatically by looping through each element in the array which is a new line from input anyway. Going across/right each time is done by multiplying the current index by 3.
+    // Because we start at the index 0 (starting position , we can call this position 1) and we add 3 each time (position 1+3 = 4th position) it is already at the right index as the 4th position is the 3rd index.
+    if (repeated_line[3 * i] === "#") {
+      trees_encountered += 1;
+    }
+  }
+
+  return trees_encountered + 1;
+}
 
 function day_3_part_2(input) {
-  return;
+  return 0;
 }
 
 const part_1_output = day_3_part_1(input_array);
