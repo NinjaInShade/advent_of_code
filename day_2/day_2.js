2b397dc32bdc79080832b1b5430236cbdf7b7cb9
Leon Michalak <leonmichalak6@gmail.com>
2020-12-22 15:54:45 +0000

    Split day 2 parsing logic into it's own function to make main function as slim as possible

--- day_2/day_2.js
+++ day_2/day_2.js
@@ -2,22 +2,34 @@ const parse_file = require("../parse_file.js");
 
 const input_array = parse_file("day_2", "day_2_input.txt");
 
+function day_2_parse(string) {
+  // Initial split parses data into an array of 3 elements. ["least-most", "letter:", "password"] - eg ["6-10", "s:", "sadvasdasfvas"]
+  // This means i will have to parse each bit of data i need.
+  const initial_split = string.split(" ");
+
+  // least_most_parse gives an array of ["least", "most"] - eg ["6", "10"]. I set up this variable to not repeat code.
+  const least_most_parse = initial_split[0].split("-");
+  const least_occurences = least_most_parse[0];
+  const most_occurences = least_most_parse[1];
+
+  // Parsing letter and password via indexing.
+  const letter = initial_split[1][0];
+  const password = initial_split[2];
+
+  // Return an object of this data.
+  return {
+    least_occurences,
+    most_occurences,
+    letter,
+    password,
+  };
+}
+
 function day_2_part_1(input) {
-  // Data comes in the format "least-most letter: password" (eg. ["6-10 s: sadvasdasfvas"]), so i have to parse it first into variables.
+  // Data comes in the format "least-most letter: password" (eg. ["6-10 s: sadvasdasfvas"]), so i have to parse it first.
 
   for (i in input) {
-    // Initial split parses data into an array of 3 elements. ["least-most", "letter:", "password"] - eg ["6-10", "s:", "sadvasdasfvas"]
-    // This means i will have to parse each bit of data i need.
-    const initial_split = input[i].split(" ");
-
-    // least_most_parse gives an array of ["least", "most"] - eg ["6", "10"]. I set up this variable to not repeat code.
-    const least_most_parse = initial_split[0].split("-");
-    const least_occurences = least_most_parse[0];
-    const most_occurences = least_most_parse[1];
-
-    // Parsing letter and password via indexing.
-    const letter = initial_split[1][0];
-    const password = initial_split[2];
+    const data = day_2_parse(input[i]);
   }
 }
 
