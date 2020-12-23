97576e0d46f58ba45c533c1246cd10ce15158d3f
Leon Michalak <leonmichalak6@gmail.com>
2020-12-23 08:31:21 +0000

    Day 3 part 2 completed and correct. Some code was refactored

--- day_3/day_3.js
+++ day_3/day_3.js
@@ -1,34 +1,59 @@
 const parse_file = require("../parse_file.js");
 
 const input_array = parse_file("day_3", "day_3_input.txt");
+// const input_array = parse_file("day_3", "test.txt");
 
-function day_3_part_1(input) {
+function day_3_part_1(input, x_slope, y_slope) {
   // Variable to keep track of tree's we hit
   let trees_encountered = 0;
 
-  // Loop through each element in array (or each line from the input file)
   for (i in input) {
-    // In challenge it says pattern on each line repeats many times, i'll do 5 for arguments sake.
+    // In challenge it says pattern on each line repeats many times, i'll do 15 for arguments sake.
     let repeated_line = input[i];
-    for (j = 0; j < 5; j++) {
+    for (j = 0; j < 15; j++) {
       repeated_line = `${repeated_line}${repeated_line}`;
     }
 
-    // Going down 1 is simulated automatically by looping through each element in the array which is a new line from input anyway. Going across/right each time is done by multiplying the current index by 3.
-    // Because we start at the index 0 (starting position , we can call this position 1) and we add 3 each time (position 1+3 = 4th position) it is already at the right index as the 4th position is the 3rd index.
-    if (repeated_line[3 * i] === "#") {
+    // This line ensures we skip any lines ( the y axis ) if its not in the slopes' times tables.
+    if (i % y_slope !== 0) {
+      continue;
+    }
+
+    // We now check the x position ( remember were on the correct y position ) by multiplying x_slope by the iteration of the loop.
+    // We have to then divide by y_slope incase the gap we've jumped in the y axis is more than one, as we dont count the x movement if we were never there, whereas just i does.
+    if (repeated_line[(x_slope * i) / y_slope] === "#") {
       trees_encountered += 1;
     }
   }
 
-  return trees_encountered + 1;
+  return trees_encountered;
 }
 
 function day_3_part_2(input) {
-  return 0;
+  // The slopes the challenge wants me to test.
+  slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ];
+
+  // Keeps a running count of each answer multiplied together.
+  let multiplication_count = 1;
+
+  for (i in slopes) {
+    // We use part 1 of this challenge to calculate each slopes trees encountered.
+    const trees_encountered = day_3_part_1(input, slopes[i][0], slopes[i][1]);
+    console.log(trees_encountered);
+
+    multiplication_count = multiplication_count * trees_encountered;
+  }
+
+  return multiplication_count;
 }
 
-const part_1_output = day_3_part_1(input_array);
+const part_1_output = day_3_part_1(input_array, 3, 1);
 const part_2_output = day_3_part_2(input_array);
 
 console.log(`Part 1: ${part_1_output}\nPart 2: ${part_2_output}`);
