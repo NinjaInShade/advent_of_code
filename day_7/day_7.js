35b001925308db035910e49a9823a8f37f8eedfa
Leon Michalak <leonmichalak6@gmail.com>
2020-12-26 13:25:52 +0000

    Day 7 boilerplate

---
+++ day_7/day_7.js
@@ -0,0 +1,16 @@
+const parse_file = require("../parse_file.js");
+
+const input_array = parse_file("day_7", "day_7_input.txt", "\r\n");
+
+function day_7_part_1(input) {
+  return 0;
+}
+
+function day_7_part_2(input) {
+  return 0;
+}
+
+const part_1_output = day_7_part_1(input_array);
+const part_2_output = day_7_part_2(input_array);
+
+console.log(`Part 1: ${part_1_output}\nPart 2: ${part_2_output}`);
