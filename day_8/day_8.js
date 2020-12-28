de6c122f24d9509ff1a804b3daf492b08514f0f8
Leon Michalak <leonmichalak6@gmail.com>
2020-12-28 14:19:27 +0000

    Tried day 8 part 2. Going to leave it for now. Also added some more comments

--- day_8/day_8.js
+++ day_8/day_8.js
@@ -2,6 +2,18 @@ const parse_file = require("../parse_file.js");
 
 const input_array = parse_file("day_8", "day_8_input.txt", "\r\n");
 
+function parse_instruction(string) {
+  // Split data in 2
+  const initial_split = string.split(" ");
+
+  // Parse the initial split to 2 useful bits of information
+  const command = initial_split[0];
+  const sign = initial_split[1][0];
+  const number = initial_split[1].slice(1);
+
+  return [command, sign, number];
+}
+
 function day_5_part_1(input) {
   // Since a jmp command jumps past certain commands, we can use a while loop and control the index ourselves.
   let cmd_line = 0;
@@ -12,18 +24,15 @@ function day_5_part_1(input) {
 
   // We use while to have control over which command(line) we read.
   while (cmd_line < input.length) {
-    // Split data in 2
-    const initial_split = input[cmd_line].split(" ");
+    // Parse line
+    const [command, sign, number] = parse_instruction(input[cmd_line]);
 
-    // Parse the initial split to 2 useful bits of information
-    const command = initial_split[0];
-    const sign = initial_split[1][0];
-    const number = initial_split[1].slice(1);
-
+    // If this cmd has been ran before and we start going into an infinite loop, break.
     if (ran_commands.includes(`${command}${sign}${number}[${cmd_line}]`)) {
       break;
     }
 
+    // If all is well push this current command to the array.
     ran_commands.push(`${command}${sign}${number}[${cmd_line}]`);
 
     if (command === "acc") {
